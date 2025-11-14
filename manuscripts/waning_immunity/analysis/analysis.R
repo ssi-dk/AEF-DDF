7437e2c261cfad17c8e8c67e54cc6245c59e0f4e
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2025-11-14 13:30:56 +0100

    WIP

--- manuscripts/waning_immunity/analysis/analysis.R
+++ manuscripts/waning_immunity/analysis/analysis.R
@@ -11,12 +11,13 @@ M_subset <- c(2, 6, 10)
 monotonous <- FALSE
 individual_level <- FALSE
 
+# Controls for the computation
+future::plan("multicore", gc = TRUE, workers = unname(future::availableCores(omit = 1)))
 
 
 
 
-
-# Lockfile should only be created when getting a new verison of diseasy
+# Lockfile should only be created when getting a new version of diseasy
 ## pak::lockfile_create("ssi-dk/diseasy", lib = "r-library", upgrade = TRUE)
 ## pak::pak("ssi-dk/diseasy", lib = "r-library")
 
@@ -39,8 +40,8 @@ if (!identical(cache$get("installation_state"), installation_state)) {
 options("diseasy.cache" = cache)
 
 # Set R to use the specific packages defined in the lockfile
-.libPaths(file.path(getwd(), "r-library"))
-stopifnot(endsWith(.libPaths()[1], "r-library"))
+#.libPaths(file.path(getwd(), "r-library"))
+#stopifnot(endsWith(.libPaths()[1], "r-library"))
 
 
 
@@ -55,25 +56,37 @@ library(diseasy)
 # and a sum of exponentials.
 
 
-im <- DiseasyImmunity$new()
-
 # Define custom waning functions
-waning_functions <- list(
+single_target_waning_functions <- list(
   "sigmoidal_waning" = \(t) exp(-(t - 1) * 6) / (1 + exp(-(t - 1) * 6)),
-  "sum_exp" = \(t) 1 / 3 * (exp(- t/2) + exp(-t) + exp(-2 * t)),
+  "sum_exp" = \(t) 1 / 2 * (exp(- t / 2) + exp(-2 * t)),
   "exponential" = \(t) exp(-t)
 )
 
+# Determine optimal optimisers per method/strategy pair
+optim_control <- diseasy::diseasy_immunity_optimiser_results |>
+  dplyr::group_by(dplyr::across(c("method", "strategy", "optim_method"))) |>
+  dplyr::summarise(
+    "value" = sum(.data$value, na.rm = TRUE),
+    .groups = "drop_last"
+  ) |>
+  dplyr::filter(.data$value > 0) |>
+  dplyr::slice_min(.data$value, with_ties = FALSE) |>
+  dplyr::mutate(
+    "optim_control" = purrr::map(.data$optim_method, ~ list("optim_method" = .))
+  ) |>
+  dplyr::select(!c("value", "optim_method"))
+
+
 # Get combinations of problems to solve
 inputs <- tidyr::expand_grid(
-  "target" = names(waning_functions),
+  "target" = names(single_target_waning_functions),
   "method-strategy" = c(
     "free_gamma-naive", "free_gamma-recursive",
     "free_delta-naive", "free_delta-recursive",
     "all_free-naive", "all_free-recursive", "all_free-combination"
   ),
-  #"M" = c(1, 2, 3, 4)
-  "M" = seq.int(from = 1, to = 10, by = 1)
+  "M" = seq.int(from = 1, to = max(M_subset), by = 1)
 ) |>
   tidyr::separate_wider_delim(
     cols = "method-strategy",
@@ -81,69 +94,78 @@ inputs <- tidyr::expand_grid(
     names = c("method", "strategy")
   ) |>
   dplyr::left_join(
-    tibble::enframe(waning_functions, name = "target", value = "waning_function"),
+    tibble::enframe(
+      single_target_waning_functions,
+      name = "target",
+      value = "waning_function"
+    ),
     by = "target"
+  ) |>
+  dplyr::left_join(
+    optim_control,
+    by = c("method", "strategy")
   )
 
 # Generate approximations (stored in the cache)
-future::plan("multisession", gc = TRUE, workers = unname(future::availableCores(omit = 1)))
 approximation_output <- furrr::future_pmap(
   .progress = TRUE,
   .options = furrr::furrr_options(seed = TRUE),
   inputs,
-  \(target, method, strategy, M, waning_function) {
-    im$set_custom_waning(
-      custom_function = waning_functions[["exponential"]],
-      target = "infection",
-      name = "exponential",
-    )
+  \(target, method, strategy, M, waning_function, optim_control) {
 
-    approx <- im$approximate_compartmental(
-      method = "all_free",
-      M = 4,
-      strategy = "combination",
-      monotonous = monotonous,
-      individual_level = individual_level
-    )
+    try({
+      im <- DiseasyImmunity$new()
 
-    list(
-      "gamma" = approx$gamma$infection,
-      "delta" = approx$delta,
-      "method" = method,
-      "strategy" = strategy,
-      "M" = M,
-      "target" = target
-    )
+      im$set_custom_waning(
+        custom_function = waning_function,
+        target = "infection",
+        name = target,
+      )
+
+      approx <- im$approximate_compartmental(
+        method = method,
+        M = M,
+        strategy = strategy,
+        monotonous = monotonous,
+        individual_level = individual_level
+      )
+
+      # Get a reference to the internal helper functions
+      private <- im$.__enclos_env__$private
+
+      # Convert gamma and delta values to plotting functions
+      modifyList(
+        approx,
+        list(
+          "target" = target,
+          "approx_function" = private$get_approximation(approx$gamma$infection, approx$delta, M)
+        )
+      )
+    })
   }
 )
 
-# Get a reference to the internal helper functions
-private <- im$.__enclos_env__$private
-
-
-# Convert gamma and delta values to plotting functions
-plotters <- purrr::map(
-  approximation_output,
-  ~ private$get_approximation(
-    purrr::pluck(.x, "gamma"),
-    purrr::pluck(.x, "delta"),
-    purrr::pluck(.x, "M")
-  )
-)
 
 # Generate data.frame for plotting
-generators <- cbind(inputs, tibble::tibble("approx_function" = plotters)) |>
+generators <- cbind(
+  inputs,
+  tibble::tibble("approx_function" = approximation_output$approx_function)
+) |>
   tibble::as_tibble()
 
 
 t <- seq(0, 3.5, by = 1 / 10)
 results <- generators |>
-  tidyr::pivot_longer(c("waning_function", "approx_function"), names_to = "type", values_to = "func") |>
+  tidyr::pivot_longer(
+    c("waning_function", "approx_function"),
+    names_to = "type",
+    values_to = "func"
+  ) |>
   tidyr::crossing(t) |>
-  dplyr::mutate(y = purrr::map2_dbl(.data$func, .data$t, \(f, t) f(t))) |>
-  dplyr::select(!c("func"))
+  dplyr::mutate("y" = purrr::map2_dbl(.data$func, .data$t, \(f, t) f(t))) |>
+  dplyr::select(!"func")
 
-appprox_results <- results |>
+approx_results <- results |>
   dplyr::filter(.data$type == "approx_function")
 
 reference_results <- results |>
@@ -153,7 +175,7 @@ reference_results <- results |>
 
 ggplot2::ggplot(mapping = ggplot2::aes(x = t, y = y, color = method)) +
   ggplot2::geom_line(
-    data = appprox_results,
+    data = approx_results,
     mapping = ggplot2::aes(linetype = strategy),
     linewidth = 1
   ) +
@@ -194,11 +216,11 @@ residuals <- dplyr::left_join(
 ggplot2::ggplot() +
   ggplot2::geom_line(
     data = dplyr::filter(
-        residuals,
-        .data$target == !!single_target,
-        .data$M %in% M_subset,
-        .data$method != "Target"
-      ),
+      residuals,
+      .data$target == !!single_target,
+      .data$M %in% M_subset,
+      .data$method != "Target"
+    ),
     mapping = ggplot2::aes(x = t, y = value, colour = method, linetype = strategy),
     linewidth = 1
   ) +
@@ -251,19 +273,10 @@ ggplot2::ggsave("../figures/1.png")
 
 
 # Create an elbow curve of total residuals
-error <- generators |>
-  #dplyr::filter(.data$target == !!single_target) |>
-  dplyr::mutate(
-    "error" = purrr::map2_dbl(
-      .x = .data$waning_function,
-      .y = .data$approx_function,
-      ~ stats::integrate(
-        \(t) (.x(t) - .y(t))^2,
-        lower = 0,
-        upper = Inf
-      )$value
-    )
-  ) |>
+error <- cbind(
+  inputs,
+  tibble::tibble("error" = approximation_output$sqrt_integral)
+) |>
   dplyr::summarise(
     "error" = sum(.data$error),
     .by = c("M", "method", "strategy")
@@ -275,7 +288,6 @@ ggplot2::ggplot() +
   ggplot2::geom_line(
     data = dplyr::filter(
       error,
-      #.data$target == !!single_target,
       .data$M > 1
     ),
     mapping = ggplot2::aes(x = M, y = error, colour = method, linetype = strategy),
@@ -302,9 +314,172 @@ ggplot2::ggplot() +
   ggplot2::labs(
     caption = paste(
       sep = "\n",
-      glue::glue("Total error from approximation to single target ({single_target})"),
+      glue::glue("Error (disregarding penalty) from approximation to single targets"),
       glue::glue("individual_level = {individual_level}, monotonous = {monotonous}")
     )
   )
 
 ggplot2::ggsave("../figures/2.png")
+
+
+
+# Define custom waning functions for dual waning optimisation
+dual_target_waning_functions <- list(
+  "sigmoidal_waning" = \(t) exp(-(t - 1) * 6) / (1 + exp(-(t - 1) * 6)),
+  "sum_exp" = \(t) 1 / 2 * (exp(- t / 2) + exp(-2 * t)),
+  "exponential" = \(t) exp(-t),
+  "sigmoidal_waning_2t" = \(t) exp(-((t / 2) - 1) * 6) / (1 + exp(-((t / 2) - 1) * 6)),
+  "sum_exp_2t" = \(t) 1 / 2 * (exp(- (t / 2) / 2) + exp(-2 * (t / 2))),
+  "exponential_2t" = \(t) exp(-(t / 2))
+)
+
+# Get combinations of problems to solve
+inputs <- tidyr::expand_grid(
+  "target_1" = names(dual_target_waning_functions),
+  "target_2" = names(dual_target_waning_functions),
+  "method-strategy" = c(
+    "free_gamma-naive", "free_gamma-recursive",
+    "free_delta-naive", "free_delta-recursive",
+    "all_free-naive", "all_free-recursive", "all_free-combination"
+  ),
+  "M" = seq.int(from = 1, to = max(M_subset), by = 1)
+) |>
+  tidyr::separate_wider_delim(
+    cols = "method-strategy",
+    delim = "-",
+    names = c("method", "strategy")
+  ) |>
+  dplyr::left_join(
+    tibble::enframe(
+      dual_target_waning_functions,
+      name = "target_1",
+      value = "waning_function_1"
+    ),
+    by = "target_1"
+  ) |>
+  dplyr::left_join(
+    tibble::enframe(
+      dual_target_waning_functions,
+      name = "target_2",
+      value = "waning_function_2"
+    ),
+    by = "target_2"
+  ) |>
+  dplyr::left_join(
+    optim_control,
+    by = c("method", "strategy")
+  )
+
+
+# Test only a single combination of the two targets (permutation does not matter)
+inputs <- inputs |>
+  dplyr::filter(.data$target_1 != .data$target_2) |>
+  dplyr::mutate(
+    "target_set" = purrr::map2_chr(
+      .x = .data$target_1,
+      .y = .data$target_2,
+      ~ toString(sort(c(.x, .y)))
+    )
+  ) |>
+  dplyr::slice_head(
+    n = 1,
+    by = c("method", "strategy", "M", "target_set")
+  ) |>
+  dplyr::select(!"target_set")
+
+
+
+approximation_output <- furrr::future_pmap(
+  .progress = TRUE,
+  .options = furrr::furrr_options(seed = TRUE),
+  inputs,
+  \(target_1, target_2, method, strategy, M, waning_function_1, waning_function_2, optim_control) {
+
+    try({
+      im <- DiseasyImmunity$new()
+
+      im$set_custom_waning(
+        custom_function = waning_function_1,
+        target = "target_1",
+        name = target_1,
+      )
+
+      im$set_custom_waning(
+        custom_function = waning_function_2,
+        target = "target_2",
+        name = target_2,
+      )
+
+      approx <- im$approximate_compartmental(
+        method = method,
+        M = M,
+        strategy = strategy,
+        monotonous = monotonous,
+        individual_level = individual_level
+      )
+
+      # Get a reference to the internal helper functions
+      private <- im$.__enclos_env__$private
+
+      modifyList(
+        approx,
+        list(
+          "target_1" = target_1,
+          "target_2" = target_2,
+          "approx_function_1" = private$get_approximation(approx$gamma$target_1, approx$delta, M),
+          "approx_function_2" = private$get_approximation(approx$gamma$target_2, approx$delta, M)
+        )
+      )
+    })
+  }
+)
+
+
+# Create an elbow curve of total residuals
+error <- cbind(
+  inputs,
+  tibble::tibble("error" = approximation_output$sqrt_integral)
+) |>
+  dplyr::summarise(
+    "error" = sum(.data$error),
+    .by = c("M", "method", "strategy")
+  )
+
+
+# Plot a subset of the data for illustrative purposes
+ggplot2::ggplot() +
+  ggplot2::geom_line(
+    data = dplyr::filter(
+      error,
+      .data$M > 1
+    ),
+    mapping = ggplot2::aes(x = M, y = error, colour = method, linetype = strategy),
+    linewidth = 1
+  ) +
+  ggplot2::guides(
+    colour = ggplot2::guide_legend(
+      title = "Method"
+    ),
+    linetype = ggplot2::guide_legend(
+      title = "Strategy"
+    )
+  ) +
+  ggplot2::scale_color_manual(
+    values = c(
+      "all_free"   = "#B51412", # SSI Red
+      "free_gamma" = "#367F68", # SSI Green
+      "free_delta" = "#3C6088"  # SSI Blue
+    )
+  ) +
+  ggplot2::labs(y = latex2exp::TeX(r"{Error $\left(\int R^2\right)$}")) +
+  ggplot2::coord_cartesian(ylim = c(0, NA), expand = FALSE) +
+  ggplot2::theme_bw() +
+  ggplot2::labs(
+    caption = paste(
+      sep = "\n",
+      glue::glue("Error (disregarding penalty) from approximation to two targets"),
+      glue::glue("individual_level = {individual_level}, monotonous = {monotonous}")
+    )
+  )
+
+ggplot2::ggsave("../figures/3.png")
