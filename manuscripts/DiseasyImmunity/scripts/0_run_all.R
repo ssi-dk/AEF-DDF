b0d5d5e65bc6bd1eeb7c6b10a54949d91a726575
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2026-08-14 11:04:38 +0200

    feat(waning_immunity): Enable immunity optimisation script

--- manuscripts/DiseasyImmunity/scripts/0_run_all.R
+++ manuscripts/DiseasyImmunity/scripts/0_run_all.R
@@ -7,5 +7,5 @@ setwd(wd)
 
 source(file.path("scripts", "1_create_offline_repo.R"))
 source(file.path("scripts", "2_libPaths.R"))
-# source(file.path("scripts", "3_diseasy_immunity_optimisation")) # We leave this commented until NGC runs are downloaded locally
+source(file.path("scripts", "3_diseasy_immunity_optimisation")) # We leave this commented until NGC runs are downloaded locally
 source(file.path("scripts", "4_0_analysis.R"))
